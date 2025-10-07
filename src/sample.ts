4b4d04d9da3306c3e85dc422373c546f0da98bba
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-10-07 20:07:43 +0800

    Update sample.ts

--- src/sample.ts
+++ src/sample.ts
@@ -1,7 +1,7 @@
 const author = {
     name: `薇薇`,
     nickname: `薇薇',
-    tiktok: `https://www.tiktok.com/@VMimi33453`,
+    tiktok: `https://www.douyin.com/@VMimi33453`,
     ig: `https://www.instagram.com/codetonero/`,
     x: `https://x.com/codetonero`,
     email: 'mailto:x@glee.ch'
