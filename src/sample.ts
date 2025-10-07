c3fb84828821b35014228465f1a760117461c93b
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-10-07 18:49:14 +0800

    Update sample.ts

--- src/sample.ts
+++ src/sample.ts
@@ -1,6 +1,6 @@
 const author = {
-    name: `Angela`,
-    nickname: `angie`,
+    name: `薇薇`,
+    nickname: `vv`,
     tiktok: `https://www.tiktok.com/@codetonero`,
     ig: `https://www.instagram.com/codetonero/`,
     x: `https://x.com/codetonero`,
